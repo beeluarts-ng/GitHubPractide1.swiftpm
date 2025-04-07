5b9cad05ec0701ea60439bf15b45cbe3d6cc64b4
Bibi <bgonzalez7140@stu.d214.org>
2025-04-07 13:33:31 -0500

    Added text field and variable.

--- ContentView.swift
+++ ContentView.swift
@@ -1,7 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var word: String
     var body: some View {
+        TextField("Type a word", text: $word)
+            .onSubmit {
+                print("\(word)")
+            }
         Text("Peter Swanson")
         Text("David")
         Text("Brennan")
